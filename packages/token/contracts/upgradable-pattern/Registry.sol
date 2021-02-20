6efc4d2c1ad5312f80aa3ab257e504f32db7ff1e
jedi <hmgarcia06@gmail.com>
2021-02-19 23:28:50 -0500

    fixes for assembly scripts

--- packages/token/contracts/upgradable-pattern/Registry.sol
+++ packages/token/contracts/upgradable-pattern/Registry.sol
@@ -20,13 +20,13 @@ contract Registry is Ownable, Storage {
           assembly {
               // Copy the data sent to the memory address starting free mem position
               let ptr := mload(0x40)
-              calldatacopy(ptr, 0, calldatasize)
+              calldatacopy(ptr, 0, calldatasize())
 
               // Proxy the call to the contract address with the provided gas and data
-              let result := delegatecall(gas, target, ptr, calldatasize, 0, 0)
+              let result := delegatecall(gas(), target, ptr, calldatasize(), 0, 0)
 
               // Copy the data returned by the proxied call to memory
-              let size := returndatasize
+              let size := returndatasize()
               returndatacopy(ptr, 0, size)
 
               // Check what the result is, return and revert accordingly
--- packages/token/contracts/upgradable-pattern/TokenV0.sol
+++ packages/token/contracts/upgradable-pattern/TokenV0.sol
@@ -3,10 +3,16 @@ pragma solidity ^0.7.0;
 
 import './Storage.sol';
 import './Ownable.sol';
+import "hardhat/console.sol";
 
 contract TokenV0 is Storage, Ownable {
+    uint public val2 = 1;
     function setVal(uint _val) public virtual returns (bool success) {
+        console.log("TESTING SETVAL");
         val = 2 * _val;
+        val2 = val2 * 3 * _val;
+        console.log("val", val);
+        console.log("val2", val2);
         return true;
     }
     
