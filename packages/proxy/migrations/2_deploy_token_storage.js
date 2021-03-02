78fc68b2701a8cda24a79436a57a8f21246bb050
jedi <hmgarcia06@gmail.com>
2021-03-01 21:17:49 -0500

    deleted old contracts

--- packages/proxy/migrations/2_deploy_token_storage.js
+++
@@ -1,9 +0,0 @@
-var BalanceSheet = artifacts.require("./BalanceSheet.sol");
-var AllowanceSheet = artifacts.require("./AllowanceSheet")
-
-module.exports = function(deployer, network, accounts) {
-  let owner = accounts[0];
-  console.log('owner of storage contracts: ' + owner)
-  deployer.deploy(BalanceSheet, {from: owner});
-  deployer.deploy(AllowanceSheet, {from: owner});
-};
--- packages/proxy/test/helpers/assertBalance.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (token, account, value) {
-	let balance = await token.balanceOf(account);
-	try {
-		assert(balance.eq(value));
-	}
-	catch(e) {
-		throw new Error("Balance was " + balance + ", expected " + value);
-	}
-}
